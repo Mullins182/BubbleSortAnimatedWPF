f2d163f9e5b1d1eb4a786af0adac8d5eb075f6cd
Mullins182 <Mullins182@t-online.de>
2024-02-24 01:03:37 +0100

    grid created (3x3), implemented bubble box textblock element, made box position responsive, added label for the box.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,6 +19,26 @@ namespace BubbleSortAnimatedWPF
         public MainWindow()
         {
             InitializeComponent();
+
+            this.SizeChanged += OnWindowSizeChanged;
+        }
+
+        private void OnWindowSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            double newWindowWidth   = e.NewSize.Width;
+            double newWindowHeight  = e.NewSize.Height;
+            //double prevWindowWidth  = e.PreviousSize.Width;
+            //double prevWindowHeight = e.PreviousSize.Height;
+
+
+            Thickness newBoxMargin  = BubbleSortBox.Margin;
+
+            newBoxMargin.Left = (newWindowWidth  / 2) - (BubbleSortBox.ActualWidth  / 2) - 8;
+            newBoxMargin.Top  = (newWindowHeight / 2) - (BubbleSortBox.ActualHeight / 2) - 19.5;
+
+            BubbleSortBox.Margin = newBoxMargin;
+
+            BubbleSortBox.Text = "\n\nW-Width: " + newWindowWidth.ToString() + "\nBox-Width: " + BubbleSortBox.Margin.ToString();
         }
     }
 }
