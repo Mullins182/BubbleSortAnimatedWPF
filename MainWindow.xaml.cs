8c194ea0e728d55ee730ab618391f84975d4a1f9
Mullins182 <Mullins182@t-online.de>
2024-02-26 23:31:21 +0100

    implemented buttons. button event properties created and button events written. New list created (sortingList).

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,34 +23,34 @@ namespace BubbleSortAnimatedWPF
             797,  950,  885,  501,  706,  84,  771,  782,  380,  916,  484,  525,  135,  495,  265,  428,  389,  662,  411,  876,  604, 650, 915, 637, 999
         };
 
+        bool bubblesort_active = false;
+
         public MainWindow()
         {
             InitializeComponent();
 
             this.SizeChanged += OnWindowSizeChanged;
-
-            BubbleSorting(unsortedIntegerList);            
         }
 
         // Event-Methode wenn Fenstergröße sich ändert ! In der Methode wird die Bubblesort Box und das Label dafür responsiv gemacht (Abhängig von Fenstergröße)
         private void OnWindowSizeChanged(object sender, SizeChangedEventArgs e)
         {
-            double newWindowWidth       = e.NewSize.Width;
-            double newWindowHeight      = e.NewSize.Height;
+            double newWindowWidth = e.NewSize.Width;
+            double newWindowHeight = e.NewSize.Height;
             //double prevWindowWidth    = e.PreviousSize.Width;
             //double prevWindowHeight   = e.PreviousSize.Height;
 
 
-            Thickness newBoxMargin      = BubbleSortBox.Margin;
-            Thickness newLabelMargin    = BubbleSortBox.Margin;
+            Thickness newBoxMargin = BubbleSortBox.Margin;
+            Thickness newLabelMargin = BubbleSortBox.Margin;
 
-            newBoxMargin.Left           = (newWindowWidth  / 2) - (BubbleSortBox.ActualWidth  / 2) - 8;
-            newBoxMargin.Top            = (newWindowHeight / 2) - (BubbleSortBox.ActualHeight / 2) - 19.5;
-            newLabelMargin.Left         = (newWindowWidth / 2)  - (BubbleSortBox.ActualWidth / 6) + 40;
-            newLabelMargin.Top          = (newWindowHeight / 2) - (BubbleSortBox.ActualHeight / 2) - 20;
+            newBoxMargin.Left = (newWindowWidth / 2) - (BubbleSortBox.ActualWidth / 2) - 330;
+            newBoxMargin.Top = (newWindowHeight / 2) - (BubbleSortBox.ActualHeight / 2) - 19.5;
+            newLabelMargin.Left = (newWindowWidth / 2) - (BubbleSortBox.ActualWidth / 6) + 40;
+            newLabelMargin.Top = (newWindowHeight / 2) - (BubbleSortBox.ActualHeight / 2) - 20;
 
-            BubbleSortBox.Margin        = newBoxMargin;
-            BubbleSortBoxLabel.Margin   = newLabelMargin;
+            BubbleSortBox.Margin = newBoxMargin;
+            BubbleSortBoxLabel.Margin = newLabelMargin;
 
             //BubbleSortBox.Text = "\n\nW-Width: " + newWindowWidth.ToString() + "\nBox-Width: " + BubbleSortBox.Margin.ToString();
         }
@@ -68,6 +68,11 @@ namespace BubbleSortAnimatedWPF
             BubbleSortBox.AppendText("\n\n");
 
             foreach (var item in unsortedIntegerList)
+            {
+                sortingList.Add(item);
+            }
+
+            foreach (var item in sortingList)
             {
 
                 if(counter % 8 == 0)
@@ -88,68 +93,97 @@ namespace BubbleSortAnimatedWPF
 
             for(int i = 0; i <= 100; i++)
             {
-                if(i % 100 == 0 && i != 0)
+                if(!bubblesort_active)
                 {
-                    TaskPercentCompleted.Content = $"Algorithmus Fortschritt: {100}%";
+                    while (!bubblesort_active)
+                    {
+                        await Task.Delay(666);
+                    }
                 }
                 else
                 {
-                    TaskPercentCompleted.Content = $"Algorithmus Fortschritt: {i % 100}%";
-                }
-
-                BubbleSortBox.Clear();
-
-                for (int x = 0; x < unsortedIntegerList.Count() - 1; x++) 
-                {
-                    buffer = unsortedIntegerList[x];
-
-                    if(x == unsortedIntegerList.Count())
+                    if(i % 100 == 0 && i != 0)
                     {
-                        continue;
+                        TaskPercentCompleted.Content = $"Algorithmus Fortschritt: {100}%";
                     }
                     else
                     {
-                        if (unsortedIntegerList[x+1] < buffer)
+                        TaskPercentCompleted.Content = $"Algorithmus Fortschritt: {i % 100}%";
+                    }
+
+                    BubbleSortBox.Clear();
+
+                    for (int x = 0; x < sortingList.Count() - 1; x++) 
+                    {
+                        buffer = sortingList[x];
+
+                        if(x == sortingList.Count())
                         {
-                            unsortedIntegerList[x]      = unsortedIntegerList[x+1];
-                            unsortedIntegerList[x+1]  = buffer;
+                            continue;
+                        }
+                        else
+                        {
+                            if (sortingList[x+1] < buffer)
+                            {
+                                sortingList[x]    = sortingList[x+1];
+                                sortingList[x+1]  = buffer;
+                            }
                         }
                     }
-                }
 
-                BubbleSortBox.AppendText("\n\n");
-                counter = 1;
+                    BubbleSortBox.AppendText("\n\n");
+                    counter = 1;
 
-                foreach (var item in unsortedIntegerList)
-                {
-                    if (counter % 8 == 0)
+                    foreach (var item in sortingList)
                     {
-                        BubbleSortBox.AppendText($"\n{item}\t");
+                        if (counter % 8 == 0)
+                        {
+                            BubbleSortBox.AppendText($"\n{item}\t");
+                        }
+
+                        BubbleSortBox.AppendText($"{item}\t");
+
+                        counter++;
                     }
 
-                    BubbleSortBox.AppendText($"{item}\t");
-
-                    counter++;
+                    await Task.Delay(200);
                 }
-
-                await Task.Delay(200);
             }
 
-            BubbleSortBoxLabel.Foreground = Brushes.OrangeRed;
+            sortingList.Clear();
+
+            BubbleSortBoxLabel.Foreground   = Brushes.OrangeRed;
+            bubblesort_active               = false;
+            start_pause_button.IsChecked    = false;
         }
 
         private void BubbleSortBox_MouseEnter(object sender, MouseEventArgs e)
         {
-            BubbleSortBox.BorderBrush = Brushes.DarkGoldenrod;
+            BubbleSortBox.BorderBrush       = Brushes.DarkGoldenrod;
+            BubbleSortBox.SelectionBrush    = Brushes.DarkGoldenrod;
             BubbleSortBox.IsInactiveSelectionHighlightEnabled = false;
-            BubbleSortBox.SelectionBrush = Brushes.DarkGoldenrod;
         }
 
         private void BubbleSortBox_MouseLeave(object sender, MouseEventArgs e)
         {
-            BubbleSortBox.BorderBrush = Brushes.DarkGoldenrod;
+            BubbleSortBox.BorderBrush       = Brushes.DarkGoldenrod;
+            BubbleSortBox.SelectionBrush    = Brushes.DarkGoldenrod;
             BubbleSortBox.IsInactiveSelectionHighlightEnabled = false;
-            BubbleSortBox.SelectionBrush = Brushes.DarkGoldenrod;
         }
+        private void start_pause_button_Checked(object sender, RoutedEventArgs e)
+        {
+            bubblesort_active = true;
+            BubbleSorting(unsortedIntegerList);
+        }
+
+        private void start_pause_button_Unchecked(object sender, RoutedEventArgs e)
+        {
+            bubblesort_active = false;
+        }
+        private void quit_program_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
     }
 }
