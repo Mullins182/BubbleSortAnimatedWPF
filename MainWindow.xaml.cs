2dc1381f450ef8a9dae10b252526046b2f811962
Mullins182 <Mullins182@t-online.de>
2024-03-02 12:35:38 +0100

    implemented random number generator and list, added buttons to frontend and events for the buttons, resized mainwindow and textbox.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Shell;
 
 namespace BubbleSortAnimatedWPF
 {
@@ -17,16 +18,20 @@ namespace BubbleSortAnimatedWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        public List<int> unsortedIntegerList = new List<int>()
-        {   505,  888,  203,  744,  216,  449,  197,  500,  284,  548,  159,  845,  999,  949,  691,  981,  324,  563,  854,  468,  117,  465,  502,  413,  232,
-            645,  848,  646,  549,  765,  225,  437,  681,  2,  44,  362,  845,  895,  627,  43,  708,  883,  950,  268,  814,  747,  689,  568,  504,  234,  781,
-            780,  574,  759,  937,  95,  761,  934,  936,  523,  459,  55,  698,  551,  111,  649,  499,  879,  77,  446,  89,  503,  917,  1,  988,  435,  681,
-            797,  885,  501,  706,  84,  771,  782,  380,  916,  484,  525,  135,  495,  265,  428,  389,  662,  411,  876,  604, 650, 915, 637, 159, 123, 182
-        };
+        //public List<int> unsortedIntegerList = new List<int>()
+        //{   505,  888,  203,  744,  216,  449,  197,  500,  284,  548,  159,  845,  999,  949,  691,  981,  324,  563,  854,  468,  117,  465,  502,  413,  232,
+        //    645,  848,  646,  549,  765,  225,  437,  681,  2,  44,  362,  845,  895,  627,  43,  708,  883,  950,  268,  814,  747,  689,  568,  504,  234,  781,
+        //    780,  574,  759,  937,  95,  761,  934,  936,  523,  459,  55,  698,  551,  111,  649,  499,  879,  77,  446,  89,  503,  917,  1,  988,  435,  681,
+        //    797,  885,  501,  706,  84,  771,  782,  380,  916,  484,  525,  135,  495,  265,  428,  389,  662,  411,  876,  604, 650, 915, 637, 159, 123, 182
+        //};
 
-        int counter             = 1;
-        bool bubblesort_active  = false;
-        bool bubblesort_paused  = false;
+        private List<int> randomIntegerList = new List<int>();
+
+        private Random generateRandoms = new Random();
+
+        //int     counter            = 1;
+        bool    bubbleSort_active  = false;
+        bool    bubbleSort_paused  = false;
 
         public MainWindow()
         {
@@ -34,22 +39,72 @@ namespace BubbleSortAnimatedWPF
 
             this.SizeChanged += OnWindowSizeChanged;
 
+            GeneratingRandomNumbers.Content = "PLEASE WAIT ... GENERATING RANDOM NUMBERS LIST !";
+        }
+
+        // Algorithm to generate a list of random Integers and output the list in Textbox
+        private async void RandomGenerator()
+        {
+            int counter         = 1;
+            int random_number   = 0;
+
+            randomIntegerList.Clear();
+
+            BubbleSortBox.Clear();
+
+            GeneratingRandomNumbers.Visibility = Visibility.Visible;
+
+            for (int rd = 100; rd > 0; rd--)
+            {
+                await Task.Delay(100);
+
+                Jump:
+                random_number = generateRandoms.Next(1, 1000);
+
+                if(randomIntegerList.Count() == 0)
+                {
+                    randomIntegerList.Add(random_number);
+                }
+                else
+                {
+                    foreach (var item in randomIntegerList)
+                    {
+                        if (random_number == item)
+                        {
+                            goto Jump;
+                        }
+                        else
+                        {
+
+                        }
+                    }
+
+                    randomIntegerList.Add(random_number);
+                }
+            }
+
+            GeneratingRandomNumbers.Visibility = Visibility.Collapsed;
+
             BubbleSortBox.AppendText("\n\n");
 
-            foreach (var item in unsortedIntegerList)
+            foreach (var item in randomIntegerList)
             {
 
                 if (counter % 9 == 0)
                 {
-                    BubbleSortBox.AppendText($"\n{item}\t");
+                    BubbleSortBox.AppendText($"\n\t{item}");
                 }
                 else
                 {
-                    BubbleSortBox.AppendText($"{item}\t");
+                    BubbleSortBox.AppendText($"\t{item}");
                 }
 
                 counter++;
             }
+
+            start_pause_button.IsEnabled    = true;
+            randoms_generate.IsEnabled      = true;
+            quit_program.IsEnabled          = true;
         }
 
         // Event-Methode wenn Fenstergröße sich ändert ! In der Methode wird die Bubblesort Box und das Label dafür responsiv gemacht (Abhängig von Fenstergröße)
@@ -61,41 +116,48 @@ namespace BubbleSortAnimatedWPF
             //double prevWindowHeight   = e.PreviousSize.Height;
 
 
-            Thickness newBoxMargin      = BubbleSortBox.Margin;
-            Thickness newLabelMargin    = BubbleSortBox.Margin;
+            Thickness newBoxMargin = BubbleSortBox.Margin;
+            Thickness newLabelMargin = BubbleSortBox.Margin;
 
-            newBoxMargin.Left = (newWindowWidth / 2) - (BubbleSortBox.ActualWidth / 2) - 200;
-            newBoxMargin.Top = (newWindowHeight / 2) - (BubbleSortBox.ActualHeight / 2) - 210;
-            newLabelMargin.Left = (newWindowWidth / 2) - (BubbleSortBox.ActualWidth / 2) + 130;
-            newLabelMargin.Top = (newWindowHeight / 2) - (BubbleSortBox.ActualHeight / 2) - 220;
+            //newBoxMargin.Left = (newWindowWidth / 2) - (BubbleSortBox.ActualWidth / 2) - 130;
+            //newBoxMargin.Top = (newWindowHeight / 2) - (BubbleSortBox.ActualHeight / 2) - 210;
+            //newLabelMargin.Left = (newWindowWidth / 2) - (BubbleSortBox.ActualWidth / 2) + 130;
+            //newLabelMargin.Top = (newWindowHeight / 2) - (BubbleSortBox.ActualHeight / 2) - 220;
 
-            BubbleSortBox.Margin = newBoxMargin;
-            BubbleSortBoxLabel.Margin = newLabelMargin;
+            //BubbleSortBox.Margin = newBoxMargin;
+            //button_stack.Margin = newBoxMargin;
+            //BubbleSortBoxLabel.Margin = newLabelMargin;
 
             //BubbleSortBox.Text = "\n\nW-Width: " + newWindowWidth.ToString() + "\nBox-Width: " + BubbleSortBox.Margin.ToString();
         }
 
-        private async void BubbleSorting(List<int> unsortedIntegerList)
+        private async void BubbleSorting(List<int> randomsList)
         {
+            randoms_generate.IsEnabled      = false;
+            quit_program.IsEnabled          = false;
+
             List<int> sortingList = new List<int>();
 
-            bubblesort_active = true;
+            int counter = 1;
+
+            bubbleSort_active = true;
 
             int buffer  = 0;
 
-            foreach (var item in unsortedIntegerList)
+            foreach (var item in randomsList)
             {
                 sortingList.Add(item);
             }
 
-            TaskPercentCompleted.Content = $"Algorithmus Fortschritt: {0}%";
+            TaskPercentCompleted.Content    = $"Algorithmus Fortschritt: {0}%";
+            TaskPercentCompleted.Visibility = Visibility.Visible;
 
             await Task.Delay(3000);
 
             for(int i = 0; i <= 100; i++)
             {
 
-                while (bubblesort_paused)
+                while (bubbleSort_paused)
                 {
                     if(BubbleSortBoxLabel.Foreground != Brushes.OrangeRed) 
                     {
@@ -141,17 +203,15 @@ namespace BubbleSortAnimatedWPF
 
                 BubbleSortBox.AppendText("\n\n");
 
-                counter = 1;
-
                 foreach (var item in sortingList)
                 {
                     if (counter % 9 == 0)
                     {
-                        BubbleSortBox.AppendText($"\n{item}\t");
+                        BubbleSortBox.AppendText($"\n\t{item}");
                     }
                     else
                     {
-                        BubbleSortBox.AppendText($"{item}\t");
+                        BubbleSortBox.AppendText($"\t{item}");
                     }
 
                     counter++;
@@ -160,12 +220,17 @@ namespace BubbleSortAnimatedWPF
                 await Task.Delay(200);
             }
 
+            await Task.Delay(4000);
+
             sortingList.Clear();
 
             BubbleSortBoxLabel.Foreground   = Brushes.OrangeRed;
-            bubblesort_active               = false;
+            bubbleSort_active               = false;
             start_pause_button.IsChecked    = false;
-            bubblesort_paused               = false;
+            bubbleSort_paused               = false;
+            TaskPercentCompleted.Visibility = Visibility.Collapsed;
+            randoms_generate.IsEnabled      = true;
+            quit_program.IsEnabled          = true;
         }
 
         private void BubbleSortBox_MouseEnter(object sender, MouseEventArgs e)
@@ -183,24 +248,34 @@ namespace BubbleSortAnimatedWPF
         }
         private void start_pause_button_Checked(object sender, RoutedEventArgs e)
         {
-            if(bubblesort_active)
+            if(bubbleSort_active)
             {
-                bubblesort_paused = false;
+                bubbleSort_paused = false;
             }
             else
             {
-                BubbleSorting(unsortedIntegerList);
+                BubbleSorting(randomIntegerList);
             }
         }
 
         private void start_pause_button_Unchecked(object sender, RoutedEventArgs e)
         {
-            bubblesort_paused = true;
+            bubbleSort_paused = true;
         }
         private void quit_program_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
 
+        private async void randoms_generate_Click(object sender, RoutedEventArgs e)
+        {
+            start_pause_button.IsEnabled    = false;
+            randoms_generate.IsEnabled      = false;
+            quit_program.IsEnabled          = false;
+
+            await Task.Delay(350);
+
+            RandomGenerator();
+        }
     }
 }
